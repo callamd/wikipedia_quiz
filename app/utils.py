a14cd38b5239de650aa49e4bf6d7bd8d0bac1706
cholet_v <cholet.victor@gmail.com>
2018-11-01 20:43:09 +0000

    Merging the retard's work

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,9 @@
 import wptools
 import requests
 
-def get_random_infobox():
+def get_random_paragrah():
     """
-    Callam
+    Retard
     Returns paragraph of text for parsing.
     """
 
@@ -16,21 +16,3 @@ def get_random_infobox():
 
     return paragraph.data["exrest"]
 
-def make_question():
-    """
-    Anton
-
-    Create a question.
-    """
-
-    return question, answer
-
-def victor_thing():
-    """
-    Victor
-    
-    Whatever you like (ascii dicks?)
-    """
-    
-    
-    return _, _
--- quiz.py
+++ quiz.py
@@ -1,37 +1,39 @@
 from question_manager import Question
+from app.utils import get_random_paragrah
 
 
-score = 0
-while for i in range(10):
-    valid = False
-    while not valid:
-        paragraph = get_paragraph()
-        question = Question(paragraph)
-        valid = question.is_valid()
+def play():
+    score = 0
+    for i in range(10):
+        valid = False
+        while not valid:
+            paragraph = get_random_paragraph()
+            question = Question(paragraph)
+            valid = question.is_valid()
+            
+        user_answer = input(question.question() + "?")
+        if question.is_correct(user_answer):
+            print("Congratulations, you're correct!")
+            score += 1
+            continue
+        else:
+            print("You stupid or something bruh?")
+            score += 0
+            continue
+            
+
     
-    user_answer = input(question.question() + "?")
-    if question.is_correct(user_answer):
-        print("Congratulations, you're correct!")
-        score += 1
-        continue
+    print("You got {} out of 10")
+    
+    if score< 5:
+        print("Idiot..")
+    elif score > 5 and score < 8:
+        print("Average")
     else:
-        print("You stupid or something bruh?")
-        score += 0
-        continue
-
-    if i >= 10:
-        break
-print("You got {} out of 10")
-
-if score< 5:
-    print("Idiot..")
-elif score > 5 and score < 8:
-    print("Average")
-else:
-    print("Genius!")
+        print("Genius!")
 
 
 
 if __name__ == "__main__":
     print("Hey Mate, wanna play ?")
-    
+    play()
