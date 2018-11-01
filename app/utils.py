5cddf314993d35db3e4f96a87a49f59a776e6f8f
Callam Delaney <cal.97g@gmail.com>
2018-11-01 20:43:51 +0000

    loop till success

--- app/utils.py
+++ app/utils.py
@@ -1,18 +1,22 @@
 import wptools
 import requests
 
-def get_random_infobox():
+def get_random_paragraph():
     """
     Callam
     Returns paragraph of text for parsing.
     """
-
-    r = requests.get("https://en.wikipedia.org/wiki/Special:Random")
-    page = r.url
-    print(page)
-    topic = r.url.split("/")[-1]
-    page = wptools.page(topic)
-    paragraph = page.get("wikitext")
+    while True:
+        try:
+            r = requests.get("https://en.wikipedia.org/wiki/Special:Random")
+            page = r.url
+            print(page)
+            topic = r.url.split("/")[-1]
+            page = wptools.page(topic)
+            paragraph = page.get("wikitext")
+            break
+        except Exception as e:
+            continue
 
     return paragraph.data["exrest"]
 
