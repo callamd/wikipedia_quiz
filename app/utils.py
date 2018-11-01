5f7c2a857374ccde12c2b3d727106f01c5bfc8f3
Callam Delaney <cal.97g@gmail.com>
2018-11-01 19:57:22 +0000

    template

---
+++ app/utils.py
@@ -0,0 +1,27 @@
+def get_random_infobox():
+    """
+    Callam
+
+    Returns infobox data in k, v format (dict)
+    """
+
+    return infobox, category
+
+
+def make_question():
+    """
+    Anton
+
+    Create a question.
+    """
+
+    return question, answer
+
+def victor_thing():
+    """
+    Victor
+
+    Whatever you like (ascii dicks?)
+    """
+
+    return _, _
